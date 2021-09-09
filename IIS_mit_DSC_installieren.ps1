fd59978a1505975ae37759933b3e91e5eba37fdf
guerayselcuk <41194728+guerayselcuk@users.noreply.github.com>
2021-09-09 17:41:37 +0200

    Webserver mit DSC
    Webserver Installation/Deinstallation mit DSC

---
+++ IIS_mit_DSC_installieren.ps1
@@ -0,0 +1,21 @@
+﻿configuration WebServerISS
+{
+    Node WebServerInstallieren
+    {
+        WindowsFeature IIS
+        {
+            Ensure               = 'Present'
+            Name                 = 'Web-Server'
+            IncludeAllSubFeature = $true
+        }
+    }
+
+    Node WebServerDeinstallieren
+    {
+        WindowsFeature IIS
+        {
+            Ensure               = 'Absent'
+            Name                 = 'Web-Server'
+        }
+    }
+}
